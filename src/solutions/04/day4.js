1793f38fb9c39d6c5b65322815a1fecc7312fb51
Adrien Pyke <adpyke@gmail.com>
2018-12-04 22:18:51 -0500

    day 4 part 1

---
+++ src/solutions/04/day4.js
@@ -0,0 +1,89 @@
+import { loadInput } from '../../util.js';
+
+const FILE = 'day4';
+
+const parseInput = () => loadInput(FILE)
+	.then(
+		data => {
+			const guards = {};
+
+			let lastId;
+			let lastTime;
+
+			data.split('\n')
+				.map(str => {
+					const match = str.match(/^\[(.*)\] (.*)/);
+					return {
+						time: new Date(match[1]),
+						cmd: match[2]
+					};
+				})
+				.sort((a, b) => a.time - b.time)
+				.forEach(({ time, cmd }) => {
+					const match = cmd.match(/#(\d+)/);
+					const id = match ? parseInt(match[1]) : null;
+
+					if (id) {
+						lastId = id;
+						if (!guards[id]) {
+							guards[id] = {
+								days: {},
+								totalSleep: 0,
+								totalAwake: 0
+							};
+						}
+					} else {
+						const date = time.toLocaleDateString();
+						if (!guards[lastId].days[date]) {
+							guards[lastId].days[date] = {
+								asleep: [],
+								awake: []
+							};
+						}
+						const day = guards[lastId].days[date];
+						const timespan = { from: lastTime, to: time };
+						if (cmd === 'wakes up') {
+							day.asleep.push(timespan);
+							guards[lastId].totalSleep += timespan.to - timespan.from;
+						} else {
+							day.awake.push(timespan);
+							guards[lastId].totalAwake += timespan.to - timespan.from;
+						}
+					}
+
+					lastTime = time;
+				});
+
+			return Object.entries(guards).map(([key, value]) => {
+				const guard = value;
+				guard.id = key;
+				return guard;
+			});
+		}
+	);
+
+const getMinute = time => (time.getHours() * 60) + time.getMinutes();
+
+export default {
+	async part1() {
+		const guard = (await parseInput()).reduce((a, b) => a.totalSleep > b.totalSleep ? a : b);
+
+		const minutes = {};
+		for (let i = 0; i < 24 * 60; i++) {
+			minutes[i] = Object.entries(guard.days)
+				.map(
+					([_, value]) => value.asleep
+						.filter(span => i >= getMinute(span.from) && i < getMinute(span.to)).length
+				).reduce((a, b) => a + b, 0);
+		}
+
+		const mostSleptMinute = Object.entries(minutes).map(([key, value]) => {
+			return { minute: parseInt(key), value };
+		}).reduce((a, b) => b.value > a.value ? b : a).minute;
+
+		return guard.id * mostSleptMinute;
+	},
+	async part2() {
+
+	}
+};
--- src/solutions/all-solutions.js
+++ src/solutions/all-solutions.js
@@ -1,5 +1,6 @@
 import day1 from './01/day1.js';
 import day2 from './02/day2.js';
 import day3 from './03/day3.js';
+import day4 from './04/day4.js';
 
-export default [day1, day2, day3];
+export default [day1, day2, day3, day4];
