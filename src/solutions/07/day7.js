741d862a073882478506a80fe7c8db48cf442c96
Adrien Pyke <adpyke@gmail.com>
2018-12-06 23:26:44 -0500

    day 7 boilerplate

---
+++ src/solutions/07/day7.js
@@ -0,0 +1,12 @@
+import { loadInput } from '../../util.js';
+
+const FILE = 'day7';
+
+const parseInput = () => loadInput(FILE);
+
+export default {
+	async part1() {
+	},
+	async part2() {
+	}
+};
--- src/solutions/all-solutions.js
+++ src/solutions/all-solutions.js
@@ -4,5 +4,6 @@ import day3 from './03/day3.js';
 import day4 from './04/day4.js';
 import day5 from './05/day5.js';
 import day6 from './06/day6.js';
+import day7 from './07/day7.js';
 
-export default [day1, day2, day3, day4, day5, day6];
+export default [day1, day2, day3, day4, day5, day6, day7];
