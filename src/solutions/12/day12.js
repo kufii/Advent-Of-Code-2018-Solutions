45d50e419cebb4e5e8ea5b7e0e4d7b84a945972b
Adrien Pyke <adpyke@gmail.com>
2018-12-12 13:48:54 -0500

    day 12 part 2

--- src/solutions/12/day12.js
+++ src/solutions/12/day12.js
@@ -15,33 +15,41 @@ const parseInput = () => {
 	return { pots, rules };
 };
 
+const run = function*(n) {
+	let { pots, rules } = parseInput();
+	const getScore = () => Object.entries(pots).filter(([_, pot]) => pot).reduce((count, [index]) => count + parseInt(index), 0);
+	for (let i = 0; i < n; i++) {
+		const toChar = bool => bool ? '#' : '.';
+		const toRule = i => toChar(pots[i - 2]) + toChar(pots[i - 1]) + toChar(pots[i]) + toChar(pots[i + 1]) + toChar(pots[i + 2]);
+		const expandBounds = () => {
+			const indexes = Object.entries(pots).filter(([_, pot]) => pot).map(([i]) => parseInt(i));
+			const min = Math.min(...indexes);
+			const max = Math.max(...indexes);
+			nTimes(i => {
+				pots[min - (i + 1)] = false;
+				pots[max + (i + 1)] = false;
+			}, 3);
+		};
+		expandBounds();
+		const newPots = {};
+		Object.entries(pots).forEach(([index]) => {
+			const key = toRule(parseInt(index));
+			const rule = rules.find(r => r.rule === key);
+			newPots[index] = rule ? rule.result : false;
+		});
+		pots = newPots;
+		yield getScore();
+	}
+};
+
 export default {
 	part1() {
-		let { pots, rules } = parseInput();
-		nTimes(() => {
-			const toChar = bool => bool ? '#' : '.';
-			const toRule = i => toChar(pots[i - 2]) + toChar(pots[i - 1]) + toChar(pots[i]) + toChar(pots[i + 1]) + toChar(pots[i + 2]);
-			const expandBounds = () => {
-				const indexes = Object.entries(pots).filter(([_, pot]) => pot).map(([i]) => parseInt(i));
-				const min = Math.min(...indexes);
-				const max = Math.max(...indexes);
-				nTimes(i => {
-					pots[min - (i + 1)] = false;
-					pots[max + (i + 1)] = false;
-				}, 3);
-			};
-			expandBounds();
-			const newPots = {};
-			Object.entries(pots).forEach(([index]) => {
-				const key = toRule(parseInt(index));
-				const rule = rules.find(r => r.rule === key);
-				newPots[index] = rule ? rule.result : false;
-			});
-			pots = newPots;
-		}, 20);
-		return Object.entries(pots).filter(([_, pot]) => pot).reduce((count, [index]) => count + parseInt(index), 0);
+		return Array.from(run(20)).pop();
 	},
 	part2() {
-		return input;
+		const firstScores = Array.from(run(200));
+		const differences = firstScores.map((value, index) => value - firstScores[index - 1] || 0);
+		const every100 = differences.slice(100, 200).reduce((a, b) => a + b, 0);
+		return firstScores[firstScores.length - 1] + (every100 * ((50000000000 - 200) / 100));
 	}
 };
