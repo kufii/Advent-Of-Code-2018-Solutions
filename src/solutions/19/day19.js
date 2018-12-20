4825310dea5d71b239ff5a6405dc776418702cb3
Adrien Pyke <adpyke@gmail.com>
2018-12-20 13:47:30 -0500

    day 19

--- src/solutions/19/day19.js
+++ src/solutions/19/day19.js
@@ -1,10 +1,78 @@
 import input from './input.js';
 
+const PSEUDO_CODE = `
+r0 = 0
+r2 = 1
+r4 = 10551326
+
+do {
+	if (r4 % r2 === 0) r0 += r2
+	r2++
+} while (r2 <= r4)
+
+return r0
+`;
+
+const ops = {
+	addr: (r, a, b) => r[a] + r[b],
+	addi: (r, a, b) => r[a] + b,
+	mulr: (r, a, b) => r[a] * r[b],
+	muli: (r, a, b) => r[a] * b,
+	banr: (r, a, b) => r[a] & r[b],
+	bani: (r, a, b) => r[a] & b,
+	borr: (r, a, b) => r[a] | r[b],
+	bori: (r, a, b) => r[a] | b,
+	setr: (r, a) => r[a],
+	seti: (r, a) => a,
+	gtir: (r, a, b) => a > r[b] ? 1 : 0,
+	gtri: (r, a, b) => r[a] > b ? 1 : 0,
+	gtrr: (r, a, b) => r[a] > r[b] ? 1 : 0,
+	eqir: (r, a, b) => a === r[b] ? 1 : 0,
+	eqri: (r, a, b) => r[a] === b ? 1 : 0,
+	eqrr: (r, a, b) => r[a] === r[b] ? 1 : 0
+};
+
+const parseInput = () => {
+	const lines = input.split('\n');
+	const bound = parseInt(lines[0].split(' ')[1]);
+	const program = lines.slice(1).map(line => {
+		const [op, a, b, c] = line.split(' ');
+		return {
+			op,
+			a: parseInt(a),
+			b: parseInt(b),
+			c: parseInt(c)
+		};
+	});
+	return { bound, program };
+};
+
 export default {
 	part1() {
-		return input;
+		const { bound, program } = parseInput();
+		const register = [0, 0, 0, 0, 0, 0];
+
+		while (register[bound] >= 0 && register[bound] < program.length) {
+			const { op, a, b, c } = program[register[bound]];
+			register[c] = ops[op](register, a, b);
+			register[bound]++;
+		}
+		return JSON.stringify(register);
 	},
 	part2() {
-		return input;
-	}
+		const solution = () => {
+			let r0 = 0;
+			let r2 = 1;
+			const r4 = 10551326;
+
+			do {
+				if (r4 % r2 === 0) r0 += r2;
+				r2++;
+			} while (r2 <= r4);
+
+			return r0;
+		};
+		return `${solution()}\nManually Solved Pseudo Code:\n<pre style='display:inline-block;text-align:left'>${PSEUDO_CODE}</span>`;
+	},
+	html: true
 };
